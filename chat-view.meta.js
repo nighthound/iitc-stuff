d00b22d985f0dab4822dde06500a08dfbdfd4d02
nighthound <nighthound@gmail.com>
2013-05-28 00:47:41 +0800

    Create chat-view.meta.js

---
+++ chat-view.meta.js
@@ -0,0 +1,13 @@
+// @id             iitc-plugin-chat-view@nighthound
+// @name           IITC plugin: Chat view on Intel
+// @version        0.1.1
+// @namespace      https://github.com/nighthound/iitc-stuff
+// @updateURL      https://github.com/nighthound/iitc-stuff/raw/master/chat-view.meta.js
+// @downloadURL    https://github.com/nighthound/iitc-stuff/raw/master/chat-view.user.js
+// @description    Toggle to only show chat box
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
