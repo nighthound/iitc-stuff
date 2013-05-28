59b4756a96dab452abdba568d040c93cd9fbfe83
nighthound <nighthound@gmail.com>
2013-05-28 16:10:14 +0800

    Create toggle-resonators-display.meta.js

---
+++ toggle-resonators-display.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id             iitc-plugin-toggle-resonators-display@nighthound
+// @name           IITC plugin: Toggle resonators display
+// @version        0.1
+// @namespace      https://github.com/nighthound/iitc-stuff
+// @updateURL      https://github.com/nighthound/iitc-stuff/raw/master/toggle-resonators-display.meta.js
+// @downloadURL    https://github.com/nighthound/iitc-stuff/raw/master/toggle-resonators-display.user.js
+// @description    Adds a button toggle to show/hide resonators
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
