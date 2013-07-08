25bb31f4250a2da3304496516c353c24de3c8411
nighthound <nighthound@gmail.com>
2013-07-08 13:49:07 +0800

    Create highlight-missing-mods.meta.js

---
+++ highlight-missing-mods.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             iitc-plugin-highlight-missing-mods@nighthound
+// @name           IITC plugin: hightlight missing mods
+// @category       Highlighter
+// @version        0.1.0
+// @namespace      https://github.com/nighthound/iitc-stuff
+// @updateURL      https://github.com/nighthound/iitc-stuff/raw/master/highlight-missing-mods.meta.js
+// @downloadURL    https://github.com/nighthound/iitc-stuff/raw/master/highlight-missing-mods.user.js
+// @description    Highlight portals with missing mods
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
