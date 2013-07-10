3f22cc2040918758d68d58ff232c226e29d8ece2
nighthound <nighthound@gmail.com>
2013-07-10 12:10:47 +0800

    Create timestamp-with-seconds.meta.js

---
+++ timestamp-with-seconds.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             iitc-plugin-timestamp-with-seconds@nighthound
+// @name           IITC plugin: timestamp with seconds
+// @category       Highlighter
+// @version        0.1.0
+// @namespace      https://github.com/nighthound/iitc-stuff
+// @updateURL      https://github.com/nighthound/iitc-stuff/raw/master/timestamp-with-seconds.meta.js
+// @downloadURL    https://github.com/nighthound/iitc-stuff/raw/master/timestamp-with-seconds.user.js
+// @description    Display log timestamps with seconds
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
